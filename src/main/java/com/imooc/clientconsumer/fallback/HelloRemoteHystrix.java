fd33e5f018525583b31be808becc1540a297094c
Ruidaimeng <13816570705@163.com>
2018-09-13 20:39:04 +0800

    加入熔断机制

---
+++ src/main/java/com/imooc/clientconsumer/fallback/HelloRemoteHystrix.java
@@ -0,0 +1,20 @@
+package com.imooc.clientconsumer.fallback;
+
+import com.imooc.clientconsumer.feign.HelloRemote;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * 熔断后的降级操作类
+ *
+ * @author ruimeng
+ * @create 2018-09-13 20:12
+ **/
+@Component
+public class HelloRemoteHystrix implements HelloRemote {
+
+    @Override
+    public String hello(@RequestParam(value = "name") String name) {
+        return "hello" +name+", this messge send failed ";
+    }
+}
