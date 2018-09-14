15ed05fb126045c7a253600d140904630b423315
Ruimeng <13816570705@163.com>
2018-09-14 16:44:11 +0800

    no

--- src/main/java/com/imooc/clientconsumer/ClientConsumerApplication.java
+++ src/main/java/com/imooc/clientconsumer/ClientConsumerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient  //启用服务注册与发现
 @EnableFeignClients  //启用feign进行远程调用
 public class ClientConsumerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ClientConsumerApplication.class, args);
     }
